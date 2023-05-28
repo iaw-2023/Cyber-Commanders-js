e5bc0edd92cd5ddfca5d60e5bd2316daea943dd1
RominaDelgado <rominasoledaddelgado1@gmail.com>
2023-05-28 20:06:10 -0300

    Agrego about page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function aboutPage(){
+    return (
+        <div>
+             <h1>About</h1>
+             <p> ctyvuinjok xrtcyvubinom xrctvyubio</p>
+        </div>
+    );
+}
+
+export default aboutPage;
