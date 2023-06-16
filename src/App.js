bf88b1b371be936fe3da9473228c9e560aa1f1d5
RominaDelgado <rominasoledaddelgado1@gmail.com>
2023-06-15 23:49:45 -0300

    fix dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 import Inicio from './Inicio';
 import Salas from './Salas';
