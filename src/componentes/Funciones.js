6cc543ee9d7bdcbc65875166d4858fd4bc1d109e
fntalmon <54511081+Federicotalmon@users.noreply.github.com>
2024-04-04 16:17:01 -0300

    Funciones responsive
    El componente de funciones y el de peliculas se muestran segun corresponda y segun el tamaño de la pantalla

--- src/componentes/Funciones.js
+++ src/componentes/Funciones.js
@@ -11,6 +11,7 @@ export default function Funciones() {
 
   const [showPeli, setShowPeli] = useState(false);
   const [funciones, setFunciones] = useState([]);
+  const [pantallaChica, setPantallaChica] = useState(false);
   const [estadoPeli, setEstadoPeli] = useState({
     id: 0,
     inicio: "",
@@ -32,14 +33,16 @@ export default function Funciones() {
 
   const [filter, setFilter] = useState("");
 
-
   const handleClick = () => {
-    setFilter('');
+    setFilter("");
   };
-
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
+  };
+
+  const toggleShowPeli = () => {
+    setShowPeli(!showPeli);
   };
 
   const filteredData = funciones.filter((funcion) =>
@@ -62,11 +65,35 @@ export default function Funciones() {
       .catch((error) => console.error(error));
   }, []);
 
+  useEffect(() => {
+    // Función para manejar cambios en el tamaño de la pantalla
+    const handleResize = () => {
+      // Condición para mostrar el div si el ancho de la pantalla es mayor que 600px
+      if (window.innerWidth < 600) {
+        setPantallaChica(true);
+      } else {
+        setPantallaChica(false);
+      }
+    };
+    // Agregar event listener para el cambio de tamaño de la ventana
+    window.addEventListener("resize", handleResize);
+
+    // Llamamos a handleResize al inicio para establecer el estado inicial
+    handleResize();
+
+    // Limpiar el event listener al desmontar el componente
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <div className="bg-gray-900 border border-gray-100 h-screen">
         <div className=" border border-gray-100 grid grid-cols-5">
-          {showPeli && <Pelis estadoPeli={estadoPeli} />}
+          {showPeli && (
+            <Pelis estadoPeli={estadoPeli} mostrarVolver={pantallaChica} toggleShowPeli={toggleShowPeli} />
+          )}
           <div
             className={
               showPeli
@@ -74,108 +101,125 @@ export default function Funciones() {
                 : "col-span-5 m-2  bg-gray-900"
             }
           >
-            <h1 className="text-center text-4xl text-slate-100">Proximas Funciones</h1>
-            <div className="flex flex-col text-white">
-              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                  <div className="flex justify-center items-center">
-                    <label className="m-2 text-lg ">Buscar por fecha:</label>
-                    <input
-                      className="m-2 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-                      type="date"
-                      value={filter}
-                      onChange={handleFilterChange}
-                      placeholder="Filtrar por nombre"
-                    />
-                    <button onClick={handleClick} className=" border-2 m-2 p-2 border-white"> Limpiar </button>
-                  </div>
-                  <div className="-white overflow-x-auto max-w-full h-screen overflow-y-scroll">
-                    <table className="text-left min-w-full overflow-auto">
-                      <thead className=" border-2 border-white  flex text-gray-100 w-full">
-                        <tr className="flex w-full mb-4 ">
-                          <th className="p-2 w-1/4">
-                            <div className="text-center font-semibold">
-                              Pelicula
-                            </div>
-                          </th>
-                          <th className="p-2 w-1/4">
-                            <div className="text-center font-semibold">
-                              Fecha
-                            </div>
-                          </th>
-                          <th className="p-2 w-1/4">
-                            <div className="text-center font-semibold">
-                              Sala
-                            </div>
-                          </th>
-                          <th className="p-2 w-1/4">
-                            <div className="text-center font-semibold">
-                              Precio
-                            </div>
-                          </th>
-                          <th className="p-2 w-1/4">
-                            <div className="text-center font-semibold">
-                              Seleccionar
-                            </div>
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="bg-grey-light flex flex-col items-center justify-between w-full">
-                        {(filteredData.length === 0  || funciones.length === 0)? (
-                          <tr>
-                            <td colSpan="2">No se encontraron resultados.</td>
-                          </tr>
-                        ) : (
-                          filteredData.map(
-                            (funcion) =>
-                              funcion.pelicula &&
-                              funcion.sala && (
-                                <tr
-                                  key={funcion.id}
-                                  className="hover:bg-gray-600 flex w-full m-2 border border-gray-100 "
-                                >
-                                  <td className="p-4 w-1/4">
-                                    <div className="text-center font-medium text-bold text-gray-100">
-                                      {funcion.pelicula.nombre}
-                                    </div>
-                                  </td>
-                                  <td className="p-2 w-1/4">
-                                    <div className="text-center text-gray-100">
-                                      {formatearFecha(funcion.inicio)}
-                                    </div>
-                                  </td>
-                                  <td className="p-2 w-1/4">
-                                    <div className="text-center text-gray-100">
-                                      {funcion.sala.nombre}
-                                    </div>
-                                  </td>
-                                  <td className="p-2 w-1/4">
-                                    <div className="text-center text-gray-100">
-                                      {funcion.precio}
-                                    </div>
-                                  </td>
-                                  <td className="p-2 w-1/4">
-                                    <div className="flex flex-col items-center justify-center">
-                                      <button
-                                        onClick={() =>
-                                          actualizarEstado({ funcion })
-                                        }
-                                        className="content-center align-center border border-gray-100 bg-transparent hover:bg-gray-100 text-gray-100 font-semibold hover:text-gray-900 py-2 px-4  rounded"
-                                      >
-                                        Seleccionar
-                                      </button>
-                                    </div>
-                                  </td>
-                                </tr>
+            {(!showPeli || !pantallaChica)  && (
+              <div>
+                <h1 className="text-center text-4xl text-slate-100">
+                  Proximas Funciones
+                </h1>
+                <div className="flex flex-col text-white">
+                  <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                      <div className="flex justify-center items-center">
+                        <label className="m-2 text-lg ">
+                          Buscar por fecha:
+                        </label>
+                        <input
+                          className="m-2 border border-gray-300 px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                          type="date"
+                          value={filter}
+                          onChange={handleFilterChange}
+                          placeholder="Filtrar por nombre"
+                        />
+                        <button
+                          onClick={handleClick}
+                          className=" border-2 m-2 p-2 border-white"
+                        >
+                          {" "}
+                          Limpiar{" "}
+                        </button>
+                      </div>
+                      <div className="-white overflow-x-auto max-w-full h-screen overflow-y-scroll">
+                        <table className="text-left min-w-full overflow-auto">
+                          <thead className=" border-2 border-white  flex text-gray-100 w-full">
+                            <tr className="flex w-full mb-4 ">
+                              <th className="p-2 w-1/4">
+                                <div className="text-center font-semibold">
+                                  Pelicula
+                                </div>
+                              </th>
+                              <th className="p-2 w-1/4">
+                                <div className="text-center font-semibold">
+                                  Fecha
+                                </div>
+                              </th>
+                              <th className="p-2 w-1/4">
+                                <div className="text-center font-semibold">
+                                  Sala
+                                </div>
+                              </th>
+                              <th className="p-2 w-1/4">
+                                <div className="text-center font-semibold">
+                                  Precio
+                                </div>
+                              </th>
+                              <th className="p-2 w-1/4">
+                                <div className="text-center font-semibold">
+                                  Seleccionar
+                                </div>
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody className="bg-grey-light flex flex-col items-center justify-between w-full">
+                            {filteredData.length === 0 ||
+                            funciones.length === 0 ? (
+                              <tr>
+                                <td colSpan="2">
+                                  No se encontraron resultados.
+                                </td>
+                              </tr>
+                            ) : (
+                              filteredData.map(
+                                (funcion) =>
+                                  funcion.pelicula &&
+                                  funcion.sala && (
+                                    <tr
+                                      key={funcion.id}
+                                      className="hover:bg-gray-600 flex w-full m-2 border border-gray-100 "
+                                    >
+                                      <td className="p-4 w-1/4">
+                                        <div className="text-center font-medium text-bold text-gray-100">
+                                          {funcion.pelicula.nombre}
+                                        </div>
+                                      </td>
+                                      <td className="p-2 w-1/4">
+                                        <div className="text-center text-gray-100">
+                                          {formatearFecha(funcion.inicio)}
+                                        </div>
+                                      </td>
+                                      <td className="p-2 w-1/4">
+                                        <div className="text-center text-gray-100">
+                                          {funcion.sala.nombre}
+                                        </div>
+                                      </td>
+                                      <td className="p-2 w-1/4">
+                                        <div className="text-center text-gray-100">
+                                          {funcion.precio}
+                                        </div>
+                                      </td>
+                                      <td className="p-2 w-1/4">
+                                        <div className="flex flex-col items-center justify-center">
+                                          <button
+                                            onClick={() =>
+                                              actualizarEstado({ funcion })
+                                            }
+                                            className="content-center align-center border border-gray-100 bg-transparent hover:bg-gray-100 text-gray-100 font-semibold hover:text-gray-900 py-2 px-4  rounded"
+                                          >
+                                            Seleccionar
+                                          </button>
+                                        </div>
+                                      </td>
+                                    </tr>
+                                  )
                               )
-                          )
-                        )}
-                      </tbody>
-                    </table>
+                            )}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
